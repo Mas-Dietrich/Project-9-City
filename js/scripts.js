b099944bc79edb1375a4493ec62092450f9ee924
Mason Dietrich <MasDietrich@icloud.com>
2023-03-29 23:23:11 -0600

    Project 9

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+const hb= document.querySelector('#hamburgerBtn');
+
+hb.addEventListener('click', ()=> {
+    document.querySelector('#primaryNav').classList.toggle('open');
+});
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    speed: 2000,
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+
+    // Autoplay
+    autoplay: {
+        delay: 5000,
+    }
+});
